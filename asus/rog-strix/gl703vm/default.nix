4dc847dadaaf40652640469f3d26016b976b96e7
Adam Yeager <smoothlikeyeager@gmail.com>
2025-05-05 20:18:38 -0400

    Add GL703VM

---
+++ asus/rog-strix/gl703vm/default.nix
@@ -0,0 +1,9 @@
+{
+  imports = [
+    ../../../common/cpu/intel/cpu-only.nix
+    ../../../common/gpu/nvidia/Pascal
+    ../../../common/pc/laptop
+    ../../../common/pc/ssd
+    ../../battery.nix
+  ]
+}
